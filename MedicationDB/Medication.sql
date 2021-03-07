e08a3c43b8580678606d1198112564763be0541f
John Carlo Gutierrez <jcgutierrez0102@live.com.ph>
2021-03-07 20:10:02 +0800

    Verifying Compatibilty

--- MedicationDB/Medication.sql
+++ MedicationDB/Medication.sql
@@ -6,3 +6,47 @@
     [Dosage] DECIMAL(7, 4) NOT NULL, 
     [Date] DATETIME NOT NULL 
 )
+GO;
+
+CREATE PROCEDURE masterStoredProcedure (@Id            INTEGER = NULL,  
+                                          @Patients    VARCHAR(50) = NULL,  
+                                          @Drug     VARCHAR(50) = NULL,  
+                                          @Dosage        DECIMAL(7, 4) = NULL,  
+                                          @Date          DATETIME = NULL,  
+                                          @StatementType NVARCHAR(20) = '')  
+AS  
+  BEGIN  
+      IF @StatementType = 'Insert'  
+        BEGIN  
+            INSERT INTO [dbo].[Medication]  
+                        ([Patients],  
+                         [Drug],  
+                         [Dosage],  
+                         [Date])  
+            VALUES     ( @Patients,  
+                         @Drug,  
+                         @Dosage,  
+                         @Date)  
+        END  
+  
+      IF @StatementType = 'Select'  
+        BEGIN  
+            SELECT *  
+            FROM   [dbo].[Medication]  
+        END  
+  
+      IF @StatementType = 'Update'  
+        BEGIN  
+            UPDATE [dbo].[Medication]  
+            SET    [Patients] = @Patients,  
+                   [Drug] = @Drug,  
+                   [Dosage] = @Dosage,  
+                   [Date] = @Date
+            WHERE  [Id] = @Id 
+        END  
+      ELSE IF @StatementType = 'Delete'  
+        BEGIN  
+            DELETE FROM [dbo].[Medication]  
+            WHERE  [Id] = @Id  
+        END  
+  END    
