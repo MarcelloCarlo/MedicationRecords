461577c2c73590cbb51aeed18e4cac0bb5af59df
John Carlo Gutierrez <jcgutierrez0102@live.com.ph>
2021-06-18 08:52:17 +0800

    Updated Medication JS structure

--- MedicationRecords/Scripts/Page/Medication.js
+++ MedicationRecords/Scripts/Page/Medication.js
@@ -1,4 +1,168 @@
-﻿$(function () {
+﻿$(document).ready(function () {
+
+    var headers = {};
+    var successMessage = "";
+    var btnDismiss = '<button type="button" class="btn btn-primary" data-dismiss="modal" id="btnClose">Ok</button>';
+
+    var MedicationObjectTask = {
+
+        init: function () {
+            var self = this;
+            self.declaration();
+            self.setEvent();
+        },
+        declaration: function () {
+            var self = this;
+
+            self.$Token = $('input[name="__RequestVerificationToken"]');
+            self.$modalContent = $("#modalContent");
+            self.$modalFooter = $("#modalFooter");
+            self.$btnSubmit = $("#btnSubmit");
+            self.$btnDelete = $('a[id^="btnDelete"]');
+            self.$btnConfirm = $("#btnConfirm");
+            self.$btnClear = $("#btnClear");
+            self.$txtDosage = $("#txtDosage");
+            self.$txtDrug = $("#txtDrug");
+            self.$txtPatients = $("#txtPatients");
+
+            switch (pageName) {
+                case "Create":
+                    self.$modalContent.html('');
+                    self.$modalContent.append('<label>Are you sure you want to add this record?</label>');
+                    successMessage = 'Record successfully saved.';
+                    FormValidation();
+                    break;
+                case "Edit":
+                    self.$modalContent.html('');
+                    self.$modalContent.append('<label>Are you sure you want to update this record?</label>');
+                    successMessage = 'Record successfully updated.';
+                    FormValidation();
+                    break;
+                case "Index":
+                    self.$modalContent.html('');
+                    self.$modalContent.append('<label>Are you sure you want to delete this record?</label>');
+                    successMessage = 'Record successfully deleted.';
+                    break;
+
+            }
+
+        },
+        setEvent: function () {
+            var self = this;
+
+            self.$btnClear.on('click', function () {
+                self.$txtDosage.val('');
+                self.$txtDrug.val('');
+                self.$txtPatients.val('');
+            });
+
+            //Submit action
+            self.$btnSubmit.on('click', function () {
+                //If the inputs are valid
+                //proceed to btnConfirm action
+                //else, throw an error
+                self.$btnConfirm.on('click', function () {
+
+                    var inputVal = {
+                        Id: (patientId == '' || patientId == null ? '0' : patientId),
+                        Dosage: self.$txtDosage.val().trim(),
+                        Drug: self.$txtDrug.val().trim(),
+                        Patients: self.$txtPatients.val().trim()
+                    };
+
+                    self.setAjaxSendEvent(inputVal);
+
+                });
+
+            });
+        },
+        setAjaxSendEvent: function (inputVal) {
+            var self = this;
+
+            headers['__RequestVerificationToken'] = self.$Token.val();
+
+            $.ajax({
+                url: window.submitFormURL,
+                type: "POST",
+                data: JSON.stringify(inputVal),
+                headers: headers,
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
+                success: function (data) {
+                    if (data.IsSuccess) {
+
+                        $("#modalFooter").html('')
+                        $("#modalFooter").append(btnDismiss);
+                        $("#modalContent").html('');
+                        $("#modalContent").append('<label>' + successMessage + '</label>');
+
+                        $("#btnClose").click(function () {
+                            setTimeout(function () {
+                                window.location.replace("/Medication");
+                            }, 1000);
+                        });
+
+                    } else {
+                        if (data.IsListResult) {
+                            var msg = "";
+
+                            for (var i = 0; i < data.Result.length; i++) {
+                                msg += "Error : " + data.Result[i] + "\n";
+                            }
+
+                            $("#modalFooter").html('')
+                            $("#modalFooter").append(btnDismiss);
+                            $("#modalContent").html('');
+                            $("#modalContent").append('<label>' + msg + '</label>');
+
+                            $("#btnClose").click(function () {
+                                setTimeout(function () {
+                                    window.location.replace("/Medication");
+                                }, 1000);
+                            });
+                        }
+                        else {
+
+                            $("#modalFooter").html('')
+                            $("#modalFooter").append(btnDismiss);
+                            $("#modalContent").html('');
+                            $("#modalContent").append('<label> Error: ' + data.Result + '</label>');
+
+                            $("#btnClose").click(function () {
+                                setTimeout(function () {
+                                    window.location.replace("/Medication");
+                                }, 1000);
+                            });
+                        }
+                    }
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    $("#modalFooter").html('')
+                    $("#modalFooter").append(btnDismiss);
+                    $("#modalContent").html('');
+                    $("#modalContent").append('<label> Error: ' + $(jqXHR.responseText).filter('title').text() + ', ' + textStatus + ', ' + errorThrown + '</label>');
+
+                    $("#btnClose").click(function () {
+                        setTimeout(function () {
+                            window.location.replace("/Medication");
+                        }, 1000);
+                    });
+                },
+            });
+        }
+    }
+
+    var InitialiseMedicationObjectTask = function () {
+        var medicationObjectTask = Object.create(MedicationObjectTask);
+        medicationObjectTask.init();
+    }
+
+    InitialiseMedicationObjectTask();
+
+});
+
+
+$(function () {
 
     DecimalOnly($("#txtDosage"));
     NameOnly($("#txtDosage"));
