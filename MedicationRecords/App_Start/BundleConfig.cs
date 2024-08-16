18c6e8a7b30699b66ab492dca59a33532610fe01
John Carlo Gutierrez <jcgutierrez0102@live.com.ph>
2024-08-17 00:55:25 +0800

    Updated Libraries and dependencies

--- MedicationRecords/App_Start/BundleConfig.cs
+++ MedicationRecords/App_Start/BundleConfig.cs
@@ -8,24 +8,27 @@ namespace MedicationRecords
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new Bundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            bundles.Add(new Bundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            bundles.Add(new Bundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap-bundle").Include(
+            bundles.Add(new Bundle("~/bundles/bootstrap-bundle").Include(
                "~/Scripts/bootstrap.bundle.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css-bundle").Include(
+            bundles.Add(new Bundle("~/bundles/moment").Include(
+   "~/Scripts/moment.js"));
+
+            bundles.Add(new Bundle("~/Content/css-bundle").Include(
                 "~/Content/bootstrap.css",
                 "~/Content/CSS/site.css"));
         }
